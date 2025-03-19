5d5d74b0d99526fffe07b59b04f3e370c289bb36
aswin <aswinnathparat@gmail.com>
2025-03-19 13:54:19 +0530

    Created Manifest and Init

---
+++ ZestyBeanz/__manifest__.py
@@ -0,0 +1,18 @@
+
+{
+    'name': 'ZestyBeanz',
+    'version': '1.0',
+    'category': 'Learning',
+    'sequence': 100,
+    'summary': 'This is for Training Odoo',
+    'description': """Learning The Odoo Framework""",
+    'website': 'https://www.zbeanztech.com/',
+    'depends': [],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'license': 'LGPL-3',
+}
+
