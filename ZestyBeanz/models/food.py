1bfd1463ff47bf24b1ecbab01694f0d7aa625505
aswin <aswinnathparat@gmail.com>
2025-03-25 18:23:45 +0530

    Form,list,csv,fields

--- ZestyBeanz/models/food.py
+++ ZestyBeanz/models/food.py
@@ -4,4 +4,15 @@ class Food(models.Model):
 	_name = "food.food"
 	_description = "Model For Food"
 
+
+	# fields
 	name = fields.Char(string="Name")
+	price = fields.Float(string="Standard Price")
+	quantity = fields.Integer(string="Amount")
+	review = fields.Text(string="Review Of The Food")
+	is_satisfied = fields.Boolean(string="Satisfied/Not")
+	check_in = fields.Date(string="Check In")
+	served_time = fields.Datetime(string="Served Time")
+	types = fields.Selection([('dine_in','Dine In'),('delivery','Delivery')])
+	images = fields.Binary(string="Images")
+	blog = fields.Html(string="Blog")
