f1d8c2ceb40694a5dc6ecd58561d965f4485f366
aswin <aswinnathparat@gmail.com>
2025-04-03 18:02:40 +0530

    Changes apr 3

--- ZestyBeanz/models/food.py
+++ ZestyBeanz/models/food.py
@@ -3,6 +3,7 @@ from odoo.exceptions import ValidationError
 class Food(models.Model):
 	_name = "food.food"
 	_description = "Model For Food"
+	_rec_name = "partner_id"
 
 
 	# fields
@@ -20,12 +21,22 @@ class Food(models.Model):
 	sale_order_id = fields.Many2one('sale.order',string="Sale Order")
 	invoice_id = fields.Many2one('account.move',string="Related Invoice")
 
-	def test_function(self):
-		# Conditional Statement
-		if self.name:
-			print("Name Field is not empty",self.name) # ExceptionHandling
-		else:
-			print("The Name field is empty")
+	_sale_created = fields.Boolean(string="Sale Order Created")
+
+	def create_sale_order(self):
+		sale_order = self.env['sale.order'].create([{
+			'partner_id':23
+			}])
+		self._sale_created = True
+		return sale_order
+
+
+	def create_invoice(self):
+		pass
+	def create_contacts(self):
+		pass
+	def create_purchase_order(self):
+		pass
 
 	# ORM Methods
 	# Create
@@ -34,31 +45,45 @@ class Food(models.Model):
 
 	@api.model
 	def create(self,vals):
-		"""
-		Before creationg
-		"""
+
+		print("Before RES")
+		print("Self is ===",self)
+		print("Vals is ===",vals)
+
 		res = super(Food,self).create(vals)
-		"""
-		After creation
-		"""
-		# res contain the created record
+
+		print("After RES")
+		print("Self is ===",self)
+		print("Vals is ===",vals)
+		print("RES is ===",res)
+
 		if res.is_satisfied == False: # checking the is_satisfield is true or not
 			res.is_satisfied = True   # assingning it true
 
 		return res
 	
 	def write(self,vals):
-		flag = 1
-		if flag ==1:
-			raise ValidationError("You are not permitted to edit the record")
 		
-		"""
-		Before writing
-		"""
+		print("Before RES")
+		print("Self is ===",self)
+		print("Vals is ===",vals)
+		print("Before  RES=== ",self.review)
 
+		print("==========================")
 		res = super(Food,self).write(vals)
-		"""
-		After edit
-		"""
+
+		print("After RES")
+		print("Self is ===",self)
+		print("Vals is ===",vals)
+		print("RES is ===",res)
+		print("After Res  === ",self.review)
+		return res
+	
+
+	def unlink(self):
+		for i in self:
+			if i.is_satisfied == True:
+				raise ValidationError("You cannot delete a Satisfied Records")
+		res = super(Food, self).unlink()
 
 		return res
